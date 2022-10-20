c3f9fd2640a7dcd3de26ca798e230356a2c5cb44
Elijah Odjegba <elijahodjegba@gmail.com>
2022-10-20 16:46:59 +0100

    Add data to data.sql

--- data.sql
+++ data.sql
@@ -29,3 +29,7 @@ VALUES
   (5, 'Charmander', '2020-02-08', 2, FALSE, -11),
   vet_clinic - # (6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7), (7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13), (8, 'Angemon', '2005-06-12', 1, TRUE, -45),
   vet_clinic - #  (9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4),  (10, 'Blossom', '1998-10-13', 3, TRUE, 17), (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+  /* Day 3*/
+
+  
